4e2fa544255ea2ea207405867ad3f26183f88fc8
Wang lanxiu <1711229172@qq.com>
2020-03-10 21:13:52 +0800

    图像线性变换

--- main.cpp
+++ main.cpp
@@ -2,34 +2,27 @@
 #include<opencv2/opencv.hpp>
 using namespace std;
 using namespace cv;
-Mat gammaTransform(Mat& srcImg, float kFractor)
+
+Mat linearTransform(Mat srcImg, float a, int b)
 {
-	unsigned char LUT[256];
-	for (int i = 0; i < 256; i++)
+	if (srcImg.empty())
 	{
-		LUT[i] = saturate_cast<uchar>(pow((float)(i / 255.0), kFractor) * 255.0f);
+		cout << "No data!" << endl;
 	}
-	Mat resultImg = srcImg.clone();
-	if (srcImg.channels() == 1)
+	const int nRows = srcImg.rows;
+	const int nCols = srcImg.cols;
+	Mat resultImg =
+		Mat::zeros(srcImg.size(), srcImg.type());
+	for (int i = 0; i < nRows; i++)
 	{
-		MatIterator_<uchar>iterator =
-			resultImg.begin<uchar>();
-		MatIterator_<uchar>iteratorEnd =
-			resultImg.end<uchar>();
-		for (; iterator!= iteratorEnd; iterator++)
-			*iterator = LUT[(*iterator)];
-	}
-	else
-	{
-		MatIterator_<Vec3b>iterator =
-			resultImg.begin<Vec3b>();
-		MatIterator_<Vec3b>iteratorEnd =
-			resultImg.end<Vec3b>();
-		for (; iterator != iteratorEnd; iterator++)
+		for (int j = 0; j < nCols; j++)
 		{
-			(*iterator)[0] = LUT[((*iterator)[0])];
-			(*iterator)[1] = LUT[((*iterator)[1])];
-			(*iterator)[2] = LUT[((*iterator)[2])];
+			for (int c = 0; i < 3; i++)
+			{
+				resultImg.at<Vec3b>(i, j)[c] =
+					saturate_cast<uchar>(a *
+					(srcImg.at<Vec3b>(i, j)[c]) + b);
+			}
 		}
 	}
 	return resultImg;
@@ -39,13 +32,11 @@ int main()
 	Mat srcImg = imread("E:\\研究生资料\\老婆\\相册\\145kb.jpg");
 	if (!srcImg.data)
 		return -1;
-	float kFractor1 = 0.3;
-	float kFractor2 = 3.0;
-	Mat result1 = gammaTransform(srcImg, kFractor1);
-	Mat result2 = gammaTransform(srcImg, kFractor2);
+	float a = 1.2;
+	int b = 10;
+	Mat new_image = linearTransform(srcImg, a, b);
 	imshow("srcImg", srcImg);
-	imshow("result1", result1);
-	imshow("result2", result2);
+	imshow("new_image", new_image);
 	waitKey(0);
 	return 0;
 }
